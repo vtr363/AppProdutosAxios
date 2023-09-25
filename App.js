aa84df228b4d9534cff264184c475cd0c381d758
vtr363 <victor.m.rocha.vr@gmail.com>
2023-09-25 19:55:08 -0300

    first

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import axios from "axios"; 
 
 export default function App() {
+  const [fromAxios, setFromAxios] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [json, setJson] = useState(null)
+
+  const goAPIProsutos = () => {
+    setFromAxios(false);
+    setLoading(true);
+  
+    axios.get(`https:172.20.192.1:3000/produtos/`)
+      .then(response => {
+        console.log(response.data);
+        setTimeout(() => {
+          setLoading(false);
+          setJson(response.data);
+          setFromAxios(true);
+          Keyboard.dismiss();
+          setOperacao(false);
+        }, 2000)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{top: 30}}>
+      <View style={{ margin: 18 }}>
+        {/* <TextInput
+          style={{ margin: 18 }}
+          onChangeText={(value) => setnomeProduto(value)}
+          placeholder="Entre com o CEP"
+        /> */}
+        <Button
+          title={'Mostrar produtos'}
+          onPress={() => { goAPIProsutos() }}
+          color='green'
+        />
+        <Text>
+          {json}
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -18,3 +57,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
